8d9b2c6824ff400124799d9154472f18f25e7a37
hfjk <123456>
2017-12-15 14:44:31 +0800

    删除sql信息

--- conf/mySql.js
+++ conf/mySql.js
@@ -1,17 +1,17 @@
 /* test */
 module.exports = {
     mysqlDev: {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'huangfu',
-        database: 'api',
-        port: 3306
+        host: '',
+        user: '',
+        password: '',
+        database: '',
+        port: ''
     },
     mysqlOnline: {
-        host: '127.0.0.1',
-        user: 'huangfu',
-        password: 'huangFu1993!',
-        database: 'blog',
-        port: 3306
+        host: '',
+        user: '',
+        password: '',
+        database: '',
+        port: ''
     }
 };
