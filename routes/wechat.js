0b278f659f51d8c8287787252fdf04ed5b308aaa
jkhuangfu <253432346@qq.com>
2019-08-07 18:52:00 +0800

    Update wechat.js
    删除多余的微信验证（此操作已放在页面渲染逻辑）

--- routes/wechat.js
+++ routes/wechat.js
@@ -1,30 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
 const signature = require('../src/dao/wechat/wx_signature/signature');
 const getOpenid = require('../src/dao/wechat/wx_opnid/index');
 
-/* 微信txt文件验证专用 txt文件放置在public文件根目录 */
-const render = () => {
-    return new Promise((resolve, reject) => {
-        fs.readFile('/MP_verify_tGmHaGktkQYkf6kr.txt', "binary", (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        })
-    })
-};
-const route = async () => {
-    let html = await render();
-    return html
-};
 router
-    .get('/MP_verify_tGmHaGktkQYkf6kr.txt', async (req, res, next) => {
-        let html = await route();
-        res.render(html);
-    })
     //微信分享获取签名
     .post('/wx_signature', (req, res, next) => {
         console.log('======发送微信签名=====');
