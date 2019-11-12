ce3e0f828c1f76a5032c9a5eee853cb705d1301e
jkhuangfu <huangfujiankang@gmail.com>
2019-11-12 10:16:49 +0800

    优化

--- src/dao/wechat/wx_opnid/index.js
+++ src/dao/wechat/wx_opnid/index.js
@@ -6,7 +6,6 @@ module.exports = {
     getOpenid: async ctx => {
         const {appid, secret, code} = reqBody(ctx);
         const url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appid}&secret=${secret}&js_code=${code}&grant_type=authorization_code`;
-        const response = await fetchData(url, 'GET');
-        ctx.body = response;
+        ctx.body = await fetchData(url, 'GET');
     }
 };
